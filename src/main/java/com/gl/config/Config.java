c47632c007bc42040e22fb258a9af1caa5fe8fb8
Chethana Nk <chethana.nk@synapse.com>
2018-05-23 15:40:49 +0530

    swagger integration

--- src/main/java/com/gl/config/Config.java
+++ src/main/java/com/gl/config/Config.java
@@ -11,6 +11,16 @@ import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.xmlpull.v1.XmlPullParserException;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.IOException;
 
 /**
  * Created by chethana.nk on 29/3/18.
@@ -21,6 +31,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableAutoConfiguration
 @EnableEurekaClient
 @SpringBootApplication
+@EnableSwagger2
 public class Config {
 
 
@@ -45,7 +56,7 @@ public class Config {
     public @Bean
     MongoDbFactory mongoDbFactory() throws Exception {
 
-        return new SimpleMongoDbFactory(new MongoClient("localhost", 27017), "pmsDb");
+        return new SimpleMongoDbFactory(new MongoClient("localhost", 27017), "lmsDb");
     }
 
     public @Bean
@@ -54,6 +65,16 @@ public class Config {
         MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
 
         return mongoTemplate;
+
+    }
+
+    @Bean
+    public Docket api() throws IOException, XmlPullParserException {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.gl.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfo("Lacation management Service Api Documentation", "Documentation automatically generated", "1.1", null, new Contact("chethana", "localhost", "chethana.nk@globallogic.com"), null, null));
 
     }
 
